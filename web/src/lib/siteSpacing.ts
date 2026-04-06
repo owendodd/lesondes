37bae1d815e5e0716ff4bbaaaa05e70b766de941
Owen Dodd <owen.dodd@gmail.com>
2026-04-06 12:44:38 +0200

    update spacing

--- web/src/lib/siteSpacing.ts
+++ web/src/lib/siteSpacing.ts
@@ -4,7 +4,7 @@ export const sitePageStackClass = 'flex flex-col'
 
 export const sitePageGapClass = 'gap-[80px] max-[740px]:gap-16'
 
-export const sitePagePaddingYClass = 'py-[80px] max-[740px]:py-8'
+export const sitePagePaddingYClass = 'py-[60px] max-[740px]:py-8'
 
 export const siteBodyTextClass =
   'text-[26px] leading-[1.1] tracking-[0.08em] filter-[url(#roughen)] max-[740px]:text-[22px]'
